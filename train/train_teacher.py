72f4891f4a1f9c6ce0a317b39965a274a97ef41a
yunjinli <yunjin.li0817@gmail.com>
2024-03-14 02:16:47 +0100

    delete some notes

--- train/train_teacher.py
+++ train/train_teacher.py
@@ -1,7 +1,7 @@
 #
 # Created on May 16 2023
 # The MIT License (MIT)
-# Copyright (c) 2023 Yun-Jin Li (Jim), Technical University Munich (TUM)
+# Copyright (c) 2023 Yun-Jin Li (Jim)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 # and associated documentation files (the "Software"), to deal in the Software without restriction,
